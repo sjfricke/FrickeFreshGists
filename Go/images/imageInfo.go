9aab5f28d1ae0996cba5e53f03930a3b81718685
sjfricke <sjfricke@wisc.edu>
2017-08-19 10:08:22 -0700

    imageInfo added

--- Go/images/imageInfo.go
+++ Go/images/imageInfo.go
@@ -1,24 +1,60 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"image"
+	"image/color"
 	_ "image/jpeg"
 	_ "image/png"
-	"reflect"
 )
 
 func main() {
-	file, error := os.Open("flower.jpg")
+
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: go run imageInfo.go <input_image>")
+	}
+		
+	file, error := os.Open(os.Args[1])
 	if error != nil { log.Fatal(error); }
 	defer file.Close()
 
-	image, _, error := image.DecodeConfig(file)
+	image, imageMime, error := image.Decode(file)
 	if error != nil { log.Fatal(error);}
 
-	log.Printf("Width: %d - Height: %d", image.Width, image.Height)
-
-	imageColor := image.ColorModel
-	log.Println(reflect.TypeOf(imageColor))
+	imageColor := image.At(0,0)
+	var imageColorType string
+	
+	switch imageColor.(type) {
+	case color.Alpha:
+		imageColorType = "Alpha"
+	case color.Alpha16:
+		imageColorType = "Alpha16"		
+	case color.CMYK:
+		imageColorType = "CMYK"
+	case color.Gray:
+		imageColorType = "Gray"
+	case color.Gray16:
+		imageColorType = "Gray16"
+	case color.NRGBA:
+		imageColorType = "NRGBA"
+	case color.NRGBA64:
+		imageColorType = "NRGBA64"
+	case color.NYCbCrA:
+		imageColorType = "NYCbCrA"
+	case color.RGBA:
+		imageColorType = "RGBA"
+	case color.RGBA64:
+		imageColorType = "RGBA64"
+	case color.YCbCr:
+		imageColorType = "YCbCr"
+	default:
+		imageColorType = "Non Supported Color"
+	}
+	
+	fmt.Printf("%s:\n", os.Args[1])
+	fmt.Printf("\tType: %s\n", imageMime)
+	fmt.Printf("\tWidth: %dpx - Height: %dpx\n", image.Bounds().Max.X, image.Bounds().Max.Y)
+	fmt.Printf("\tColor: %s\n", imageColorType) 
 }
