8a41a2353b3a8746cdd90d6d50d3a21ef997a640
sjfricke <sjfricke@wisc.edu>
2018-01-12 04:56:57 -0600

    working basic parse

--- Rust/json_reader/src/main.rs
+++ Rust/json_reader/src/main.rs
@@ -1,3 +1,20 @@
+extern crate rustc_serialize;
+use rustc_serialize::json::Json;
+use std::fs::File;
+use std::io::Read;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+
+    let args: Vec<String> = env::args().collect();
+
+    println!("arg length {}", args.len());
+    
+    let mut file = File::open(&args[1]).unwrap();
+    let mut data = String::new();
+    file.read_to_string(&mut data).unwrap();
+
+    let json = Json::from_str(&data).unwrap();
+//    println!("{}", &data[0..20]);
+    println!("{}", json.find_path(&["asset", "version"]).unwrap());
 }
